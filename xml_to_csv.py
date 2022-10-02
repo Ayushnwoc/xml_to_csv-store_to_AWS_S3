59eebaf125a8f07a870a1014cb23cfec41c20aff
Ayushnwoc <75082634+Ayushnwoc@users.noreply.github.com>
2022-10-02 23:35:52 +0530

    removed unused libraries

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -2,10 +2,7 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
 import csv
-import boto
-import boto.s3.connection
 import pandas as pd
-from io import StringIO
 
 # Storing start time
 start_time = datetime.now()
@@ -65,31 +62,6 @@ with open('output.csv', 'w') as f:
             row.append("NA")
         write.writerow(row)
 
-# Now upload to AWS S3 bucket
-# we will need aws access key id and its secret key
-access_key = 'fake_key'
-secret_key = 'fake_pass'
-
-# getting bucket object
-conn = boto.connect_s3(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key)
-
-# creating bucket
-bucket_csv = conn.create_bucket('store_csv')
-
-#reading csv file and uploading to aws s3 bucket
-dataf = pd.read_csv('output.csv')
-s3 = boto3.client('s3',
-                  aws_access_key_id=access_key,
-                  aws_secret_access_key=secret_key)
-
-csv_buf = StringIO()
-dataf.to_csv(csv_buf,header=True,index=False)
-csv_buf.seek(0)
-
-#store file as store.csv
-s3.put_pbject(Bucket = 'store_csv' , Body = csv_buf.getvalue() , Key = 'store.csv')
 
 
 # storing end time
